a6026b48f9e3fd54536984d778ab1cfa4e11a50b
PeLaszlo <166426013+PeLaszlo@users.noreply.github.com>
2024-09-28 19:02:48 +0200

    feltöltés2

--- App.jsx
+++ App.jsx
@@ -1,6 +1,24 @@
 
-import Header from "./Header.jsx"
+
+import {BrowserRouter} from "react-router-dom";
+import QuestionProvider from "./QuestinProvider.jsx";
+import {Route,Routes,Outlet} from "react-router-dom";
+import Result from "./Result.jsx";
+
+import Quiz from "./Quiz.jsx";
+
+import "./Style.css";
+
+
 export default function App()
 {
-return <Header/>
+return (
+<BrowserRouter>
+
+
+<QuestionProvider>
+  <Quiz></Quiz>
+</QuestionProvider>
+</BrowserRouter>
+)
 }
